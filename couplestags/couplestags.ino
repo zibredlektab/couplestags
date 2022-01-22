37b9f36af67be91368a78fe9126fa656c4ae1ec6
Max Batchelder <max@first.ac>
2022-01-22 12:33:49 -0500

    Implemented oled updates, combined elements into arrays to simplify routines

--- couplestags/couplestags.ino
+++ couplestags/couplestags.ino
@@ -1,5 +1,4 @@
 #include <SparkFun_I2C_Mux_Arduino_Library.h>
-
 #include <Adafruit_SSD1305.h>
 #include <splash.h>
 #include <Adafruit_GrayOLED.h>
@@ -8,41 +7,15 @@
 #include <Adafruit_SPITFT.h>
 #include <Adafruit_SPITFT_Macros.h>
 
-// Adafruit IO Multiple Feed Example
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
-
-/************************** Configuration ***********************************/
-
-// edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
 #include "config.h"
 
-Adafruit_SSD1305 aoled(128, 32);
-Adafruit_SSD1305 boled(128, 32);
-Adafruit_SSD1305 coled(128, 32);
-Adafruit_SSD1305 doled(128, 32);
+Adafruit_SSD1305 oleds[4] {{128, 32}, {128, 32}, {128, 32}, {128, 32}};
 
 QWIICMUX mux;
 
-AdafruitIO_Feed *AOp = io.feed("couples-a-op");
-AdafruitIO_Feed *BOp = io.feed("couples-b-op");
-AdafruitIO_Feed *COp = io.feed("couples-c-op");
-AdafruitIO_Feed *DOp = io.feed("couples-d-op");
+AdafruitIO_Feed *feeds[4] = {io.feed("couples-a-op"), io.feed("couples-b-op"), io.feed("couples-c-op"), io.feed("couples-d-op")}; 
 
-char aname[16];
-char bname[16];
-char cname[16];
-char dname[16];
+char names[4][16];
 
 void setup() {
 
@@ -52,70 +25,41 @@ void setup() {
   // wait for serial monitor to open
   while(! Serial);
 
-  if (mux.begin() == false) {
+  if (!mux.begin()) {
     Serial.println("Mux not detected.");
     while (1);
   }
 
-  mux.setPort(1);
-  if(!aoled.begin(0x3C, 0)) {
-    Serial.println(F("A SSD1305 allocation failed"));
-    while(1);
-  } else {
-    Serial.println(F("A SSD1305 allocation succeeded"));
-  }
-  
-  mux.setPort(2);
-  if(!boled.begin(0x3C, 0)) {
-    Serial.println(F("B SSD1305 allocation failed"));
-    while(1);
-  } else {
-    Serial.println(F("B SSD1305 allocation succeeded"));
-  }
-
-  mux.setPort(3);
-  if(!coled.begin(0x3C, 0)) {
-    Serial.println(F("C SSD1305 allocation failed"));
-    while(1);
-  } else {
-    Serial.println(F("C SSD1305 allocation succeeded"));
-  }
-  
-  mux.setPort(4);
-  if(!doled.begin(0x3C, 0)) {
-    Serial.println(F("D SSD1305 allocation failed"));
-    while(1);
-  } else {
-    Serial.println(F("D SSD1305 allocation succeeded"));
-  }
-
   Serial.print("Connecting to Adafruit IO");
-
-  // connect to io.adafruit.com
   io.connect();
-
-  // attach message handler
-  AOp->onMessage(handleChange);
-  BOp->onMessage(handleChange);
-  COp->onMessage(handleChange);
-  DOp->onMessage(handleChange);
-
+  
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-
+  
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
 
-  // make sure all feeds get their current values right away
-  AOp->get();
-  BOp->get();
-  COp->get();
-  DOp->get();
 
+  for (int i = 0; i < 4; i++) {
+    mux.setPort(i);
+    if (!oleds[i].begin(0x3c, 0)) {
+      Serial.print("Oled on port ");
+      Serial.print(i+1);
+      Serial.println(" failed to initialize");
+      while(1);
+    } else {
+      Serial.print("Oled on port ");
+      Serial.print(i);
+      Serial.println(" initialized");
+    }
+
+    feeds[i]->onMessage(handleChange);
+    feeds[i]->get();
+  }
 }
 
 void loop() {
@@ -128,33 +72,39 @@ void loop() {
 void handleChange(AdafruitIO_Data *data) {
 
   Serial.print("Name of ");
-
-  // since we are using the same function to handle
-  // messages for two feeds, we can use feedName() in
-  // order to find out which feed the message came from.
   Serial.print(data->feedName());
   Serial.print(" is now: ");
-
   Serial.println(data->value());
 
-  char* nametochange;
-
   if (strcmp(data->feedName(), "couples-a-op") == 0) {
-    nametochange = aname;
+    updateOled('A', data->value());
   } else if (strcmp(data->feedName(), "couples-b-op") == 0) {
-    nametochange = bname;
+    updateOled('B', data->value());
   } else if (strcmp(data->feedName(), "couples-c-op") == 0) {
-    nametochange = cname;
+    updateOled('C', data->value());
   } else if (strcmp(data->feedName(), "couples-d-op") == 0) {
-    nametochange = dname;
+    updateOled('D', data->value());
   }
+  
+}
 
+void updateOled(const char cam, char* newname) {
+
+  int port = toupper(cam) - 65;
 
   for (int i = 0; i < 16; i++) {
-    nametochange[i] = data->value()[i];
-    if (data->value()[i] == '\0') {
+    names[port][i] = newname[i];
+    if (newname[i] == '\0') {
       break;
     }
   }
 
+  mux.setPort(port);
+  oleds[port].clearDisplay();
+  oleds[port].setCursor(0,10);
+  oleds[port].print(toupper(cam));
+  oleds[port].print(": ");
+  oleds[port].print(names[port]);
+  oleds[port].display();
+  
 }
